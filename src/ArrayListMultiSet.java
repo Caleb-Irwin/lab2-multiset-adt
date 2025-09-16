1d2dcd359d6fc9e2cc530c699d07a346c42b41bf
Caleb Irwin <me@calebirwin.ca>
2025-09-16 12:38:43 -0400

    add features add and size

--- src/ArrayListMultiSet.java
+++ src/ArrayListMultiSet.java
@@ -12,7 +12,7 @@ public class ArrayListMultiSet extends MultiSet {
      */
     @Override
     void add(int item) {
-
+        lst.add(item);
     }
 
     /**
@@ -61,6 +61,6 @@ public class ArrayListMultiSet extends MultiSet {
      */
     @Override
     int size() {
-        return -1;
+        return lst.size();
     }
 }
